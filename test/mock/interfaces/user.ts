85045f3e77b61639151f9726185a6dc79f17353d
Manuel Tati <manuel.luemba.tati@atlantico.ao>
2022-08-04 16:53:31 +0100

    add user mock repository

---
+++ test/mock/interfaces/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+  id?: string | number;
+  firstName: string;
+  lastName: string;
+  maidenName: string;
+  gender: string;
+  age: number;
+  phone: string;
+  birthDate: string;
+  bloodGroup: string;
+  username?: string | undefined;
+  email: string;
+  password: string;
+  image: string;
+}
