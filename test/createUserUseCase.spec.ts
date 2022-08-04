55c81b1a6682feed2c3753c4d96ce6d8d58a9fe8
Manuel Tati <manuel.luemba.tati@atlantico.ao>
2022-08-04 16:51:45 +0100

    fix test create user

--- test/createUserUseCase.spec.ts
+++ test/createUserUseCase.spec.ts
@@ -1,11 +1,22 @@
+import { User } from "./mock/interfaces/user";
+import users from "./mock/users/users";
+
 describe("CREATE USER", () => {
   test("should create user", () => {
     const user: User = {
       id: Math.floor(Math.random() * 100000),
-      firstName: "Kaillan",
-      lastName: "Tumelia",
-      email: "kaillan@test.com",
-      password: "123",
+      firstName: "Eleanora",
+      lastName: "Price",
+      maidenName: "Cummings",
+      age: 37,
+      gender: "female",
+      email: "umcgourty9@jalbum.net",
+      phone: "+60 184 408 0824",
+      username: "umcgourty9",
+      password: "i0xzpX",
+      birthDate: "1958-08-11",
+      image: "https://robohash.org/aliquamcumqueiure.png",
+      bloodGroup: "O+",
     };
     const createUser = createUserUseCase(user);
     expect(createUser).toBe(user);
@@ -14,10 +25,18 @@ describe("CREATE USER", () => {
   test("should not create if there is already a user with that email", () => {
     const user: User = {
       id: Math.floor(Math.random() * 100000),
-      firstName: "User003",
-      lastName: "Test",
-      email: "test001@test.com",
-      password: "123",
+      firstName: "Demetrius",
+      lastName: "Corkery",
+      maidenName: "Gleason",
+      age: 22,
+      gender: "male",
+      email: "nloiterton8@aol.com",
+      phone: "+86 356 590 9727",
+      username: "nloiterton8",
+      password: "HTQxxXV9Bq4",
+      birthDate: "1971-03-11",
+      image: "https://robohash.org/excepturiiuremolestiae.png",
+      bloodGroup: "A+",
     };
 
     expect(() => createUserUseCase(user)).toThrow(
@@ -25,32 +44,6 @@ describe("CREATE USER", () => {
     );
   });
 });
-
-interface User {
-  id?: string | number;
-  firstName: string;
-  lastName: string;
-  username?: string | undefined;
-  email: string;
-  password: string;
-}
-
-const users: User[] = [
-  {
-    id: Math.floor(Math.random() * 100000),
-    firstName: "Test001",
-    lastName: "User",
-    email: "test001@test.com",
-    password: "123",
-  },
-  {
-    id: Math.floor(Math.random() * 100000),
-    firstName: "Test002",
-    lastName: "User",
-    email: "test002@test.com",
-    password: "123",
-  },
-];
 
 const createUserUseCase = (user: User) => {
   user.username = generateUserName(user.firstName, user.lastName);
